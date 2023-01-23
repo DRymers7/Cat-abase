461f8eea91b68527363b3463b1b6d94ad5b30b24
Derek Rymers <rymersd@gmail.com>
2023-01-23 16:41:08 -0500

    Continuing DAO

--- server/src/main/java/com/catabase/server/dao/jdbc/JdbcUserDao.java
+++ server/src/main/java/com/catabase/server/dao/jdbc/JdbcUserDao.java
@@ -1,4 +1,17 @@
 package com.catabase.server.dao.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcUserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcUserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    
+
 }
--- server/src/main/java/com/catabase/server/services/appservices/UserService.java
+++ server/src/main/java/com/catabase/server/services/appservices/UserService.java
@@ -1,4 +1,4 @@
-package com.catabase.server.services;
+package com.catabase.server.services.appservices;
 
 import org.springframework.stereotype.Service;
 
