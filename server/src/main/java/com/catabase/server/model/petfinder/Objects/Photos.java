d2b286c06eb7b7f557d649dd3a4c313b9fb60608
GoFreecs <avaneeshugk20596@gmail.com>
2023-01-27 22:14:05 +0530

    pets objct created

---
+++ server/src/main/java/com/catabase/server/model/petfinder/Objects/Photos.java
@@ -0,0 +1,20 @@
+package com.catabase.server.model.petfinder.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Photos {
+    @JsonProperty("small")
+    private String small;
+    @JsonProperty("medium")
+    private String medium;
+    @JsonProperty("large")
+    private String large;
+    @JsonProperty("full")
+    private String full;
+}
