22165f8b41412ff3071cae2c4fc6a6eb1d4af25d
Derek Rymers <rymersd@gmail.com>
2023-02-07 12:10:11 -0500

    mock data wserver project addition

---
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/MockdataserverApplication.java
@@ -0,0 +1,13 @@
+package com.catabase.mockdataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockdataserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockdataserverApplication.class, args);
+	}
+
+}
---
+++ mockdataserver/src/test/java/com/catabase/mockdataserver/MockdataserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catabase.mockdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockdataserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
