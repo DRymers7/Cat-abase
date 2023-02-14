1ea020edb5090d878ea593f0592bdf941e6bb62f
Derek Rymers <rymersd@gmail.com>
2023-02-14 16:53:33 -0500

    Pausing for the evening

---
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/PetObjectFactory.java
@@ -0,0 +1,2 @@
+package com.catabase.mockdataserver.factoryimplementation;public class PetObjectFactory {
+}
---
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/objectfactoryimplementation/ContactGenerator.java
@@ -0,0 +1,15 @@
+package com.catabase.mockdataserver.factoryimplementation.objectfactoryimplementation;
+
+import com.catabase.mockdataserver.factoryimplementation.PetObjectFactory;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import org.springframework.context.annotation.Scope;
+
+@Scope()
+public class ContactFactory {
+
+
+
+
+
+}
