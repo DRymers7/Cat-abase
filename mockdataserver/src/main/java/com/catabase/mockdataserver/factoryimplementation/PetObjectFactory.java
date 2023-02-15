2c7d0f25d454c27076e64c3e743f861ec7f3f12a
Derek Rymers <rymersd@gmail.com>
2023-02-14 20:58:22 -0500

    Working on proper data gen

--- mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/PetObjectFactory.java
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/PetObjectFactory.java
@@ -1,2 +1,21 @@
-package com.catabase.mockdataserver.factoryimplementation;public class PetObjectFactory {
+package com.catabase.mockdataserver.factoryimplementation;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class PetObjectFactory {
+
+    private final FakeValuesService fakeValuesService =
+            new FakeValuesService(new Locale("en-US"), new RandomService());
+
+    private final Faker usFaker =
+            new Faker(new Locale("en-US"));
+
+
+
 }
--- mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/objectfactoryimplementation/ContactGenerator.java
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/factoryimplementation/objectfactoryimplementation/ContactGenerator.java
@@ -1,12 +1,9 @@
 package com.catabase.mockdataserver.factoryimplementation.objectfactoryimplementation;
 
-import com.catabase.mockdataserver.factoryimplementation.PetObjectFactory;
-import com.github.javafaker.Faker;
-import com.github.javafaker.service.FakeValuesService;
 import org.springframework.context.annotation.Scope;
 
 @Scope()
-public class ContactFactory {
+public class ContactGenerator {
 
 
 
