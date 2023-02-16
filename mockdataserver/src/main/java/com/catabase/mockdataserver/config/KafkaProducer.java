323bc28f4ca8570fd9caa7a7aa9dbb6fcdfbb7e6
Derek Rymers <rymersd@gmail.com>
2023-02-16 16:39:27 -0500

    Server working with container

--- mockdataserver/src/main/java/com/catabase/mockdataserver/config/KafkaProducer.java
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/config/KafkaProducer.java
@@ -1,2 +1,25 @@
-package com.catabase.mockdataserver.config;public class KafkaProducer {
+package com.catabase.mockdataserver.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+
+        try {
+            kafkaTemplate.send("cat-data", message);
+        } catch (Exception e) {
+            log.info("Failed: " + e.getMessage());
+        }
+
+    }
+
 }
