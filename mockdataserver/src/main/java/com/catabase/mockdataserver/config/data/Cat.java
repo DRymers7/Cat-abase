36fa0eae0d37f73b3ddf866674934b8c9f187efb
Derek Rymers <rymersd@gmail.com>
2023-02-18 20:38:03 -0500

    h2 config almost done, testing

--- mockdataserver/src/main/java/com/catabase/mockdataserver/config/data/Cat.java
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/config/data/Cat.java
@@ -1,5 +1,21 @@
 package com.catabase.mockdataserver.config.data;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
 public class Cat {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false)
+    private String breed;
+
+    @Column(nullable = false)
+    private String code;
+
 }
--- mockdataserver/src/main/java/com/catabase/mockdataserver/config/data/Dog.java
+++ mockdataserver/src/main/java/com/catabase/mockdataserver/config/data/Dog.java
@@ -1,4 +1,18 @@
 package com.catabase.mockdataserver.config.data;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Dog {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false)
+    private String Breed;
+
 }
